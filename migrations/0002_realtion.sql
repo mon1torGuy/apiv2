502ab94567ab83daf9f764a8a0b89839bfb97876
Pablo Camino <pcaminog@gmail.com>
2024-07-27 19:53:54 -0500

    s

---
+++ migrations/0002_realtion.sql
@@ -0,0 +1,224 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "apiKey" TEXT,
+    "plan" TEXT NOT NULL,
+    "clerkId" TEXT,
+    "primary_email" TEXT NOT NULL,
+    "subscription_id" TEXT NOT NULL,
+    "subscription_email" TEXT NOT NULL,
+    "period" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "UserPending" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "password" TEXT,
+    "token" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "UserForgot" (
+    "userId" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "token" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "UserForgot_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "apiKey" TEXT,
+    "avatar" TEXT,
+    "password" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "AccountUser" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "accId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "AccountUser_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "AccountUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "accId" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "expires" INTEGER NOT NULL DEFAULT 0,
+    "ratelimit" TEXT,
+    "refill" TEXT,
+    "prefix" TEXT,
+    "keyType" TEXT NOT NULL,
+    "jwk" TEXT,
+    "remaining" INTEGER,
+    "byteLength" INTEGER NOT NULL DEFAULT 32,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Application_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Key" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "appId" TEXT NOT NULL,
+    "accId" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "byteLength" INTEGER NOT NULL DEFAULT 135,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "environment" TEXT NOT NULL DEFAULT 'default',
+    "expires" INTEGER NOT NULL DEFAULT 0,
+    "metadata" TEXT,
+    "ratelimit" TEXT,
+    "refill" TEXT,
+    "remaining" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Key_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Key_appId_fkey" FOREIGN KEY ("appId") REFERENCES "Application" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Dnsmonitor" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "accId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "ips" TEXT NOT NULL,
+    "mon_status" BOOLEAN NOT NULL DEFAULT true,
+    "hostname" TEXT NOT NULL,
+    "checks_down" INTEGER NOT NULL,
+    "checks_up" INTEGER NOT NULL,
+    "dns_error" TEXT NOT NULL,
+    "interval" INTEGER NOT NULL,
+    "europe" BOOLEAN NOT NULL DEFAULT false,
+    "america" BOOLEAN NOT NULL DEFAULT true,
+    "asia" BOOLEAN NOT NULL DEFAULT false,
+    "middle" BOOLEAN NOT NULL DEFAULT false,
+    "isStatusPage" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Dnsmonitor_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Httpmonitor" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "accId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "status_code" INTEGER NOT NULL,
+    "mon_status" BOOLEAN NOT NULL DEFAULT true,
+    "ssl_verify" BOOLEAN NOT NULL DEFAULT true,
+    "follow_redir" BOOLEAN NOT NULL DEFAULT true,
+    "method" TEXT NOT NULL,
+    "req_timeout" INTEGER NOT NULL,
+    "req_headers" TEXT NOT NULL,
+    "interval" INTEGER NOT NULL,
+    "checks_down" INTEGER NOT NULL,
+    "checks_up" INTEGER NOT NULL,
+    "europe" BOOLEAN NOT NULL DEFAULT false,
+    "america" BOOLEAN NOT NULL DEFAULT true,
+    "asia" BOOLEAN NOT NULL DEFAULT false,
+    "middle" BOOLEAN NOT NULL DEFAULT false,
+    "isStatusPage" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Httpmonitor_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Tcpmonitor" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "accId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "ip_address" TEXT NOT NULL,
+    "port" INTEGER NOT NULL,
+    "mon_status" BOOLEAN NOT NULL DEFAULT true,
+    "interval" INTEGER NOT NULL,
+    "checks_down" INTEGER NOT NULL,
+    "checks_up" INTEGER NOT NULL,
+    "europe" BOOLEAN NOT NULL DEFAULT false,
+    "america" BOOLEAN NOT NULL DEFAULT true,
+    "asia" BOOLEAN NOT NULL DEFAULT false,
+    "middle" BOOLEAN NOT NULL DEFAULT false,
+    "isStatusPage" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Tcpmonitor_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "SSLcertificate" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "accId" TEXT NOT NULL,
+    "monId" TEXT NOT NULL,
+    "certificate" TEXT NOT NULL,
+    "expired_at" INTEGER NOT NULL,
+    "checked_at" INTEGER NOT NULL,
+    CONSTRAINT "SSLcertificate_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "SSLcertificate_monId_fkey" FOREIGN KEY ("monId") REFERENCES "Httpmonitor" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "upvote" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Jwks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "accId" TEXT NOT NULL,
+    "appID" TEXT,
+    "jwk" TEXT NOT NULL,
+    "jwtEndpoint" TEXT,
+    "lastChekcked" INTEGER NOT NULL,
+    "kid" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Jwks_accId_fkey" FOREIGN KEY ("accId") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_primary_email_key" ON "Account"("primary_email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPending_email_key" ON "UserPending"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserForgot_email_key" ON "UserForgot"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AccountUser_accId_userId_key" ON "AccountUser"("accId", "userId");
--- src/userAuthentication.ts
+++ src/userAuthentication.ts
@@ -39,7 +39,7 @@ app.post('/signup', authMiddleware, async (c) => {
 		const userCheck = await prisma.user.findUnique({
 			where: { email: email },
 		});
-
+		console.log(userCheck);
 		if (userCheck) {
 			return c.json({ success: true, message: 'User already exists', data: [] }, 400);
 		}
@@ -89,6 +89,153 @@ app.post('/signup', authMiddleware, async (c) => {
 		});
 
 		return c.json({ success: true, message: 'Validation created successfully', data: [] }, 200);
+	} catch (error) {
+		if (error instanceof SyntaxError) {
+			return c.json({ success: false, message: 'Invalid JSON syntax', data: [] }, 400);
+		} else if (error instanceof PrismaClientKnownRequestError) {
+			console.error('Prisma database error:', error);
+			return c.json({ success: false, message: 'Database error', data: [] }, 500);
+		} else {
+			console.error('Unexpected error:', error);
+			return c.json({ success: false, message: 'Internal server error', data: [] }, 500);
+		}
+	}
+});
+
+app.post('/login', authMiddleware, async (c) => {
+	const prisma = createPrismaClient(c.env.DB);
+
+	try {
+		const email = c.req.query('email');
+		const password = c.req.query('password');
+
+		if (!email || !password) {
+			return c.json({ success: true, message: 'Invalid email or password. Please check your credentials and try again', data: [] }, 400);
+		}
+		const hash = await hashPassword(password);
+
+		const user = await prisma.user.findUnique({
+			where: { email: email, password: hash },
+		});
+		if (!user) {
+			return c.json({ success: true, message: 'Invalid email or password. Please check your credentials and try again.', data: [] }, 400);
+		}
+
+		const accountInfo = await prisma.accountUser.findMany({
+			where: { userId: user.id, email: user.email },
+		});
+
+		return c.json({ success: true, message: 'User found successfully', data: accountInfo }, 200);
+	} catch (error) {
+		if (error instanceof SyntaxError) {
+			return c.json({ success: false, message: 'Invalid JSON syntax', data: [] }, 400);
+		} else if (error instanceof PrismaClientKnownRequestError) {
+			console.error('Prisma database error:', error);
+			return c.json({ success: false, message: 'Database error', data: [] }, 500);
+		} else {
+			console.error('Unexpected error:', error);
+			return c.json({ success: false, message: 'Internal server error', data: [] }, 500);
+		}
+	}
+});
+app.post('/forgot-password', authMiddleware, async (c) => {
+	const prisma = createPrismaClient(c.env.DB);
+
+	try {
+		const email = c.req.query('email');
+
+		if (!email) {
+			return c.json({ success: true, message: 'Invalid email. Please check your credentials and try again', data: [] }, 400);
+		}
+
+		const user = await prisma.user.findUnique({
+			where: { email: email },
+		});
+		if (!user) {
+			return c.json({ success: true, message: 'Invalid email. Please check your credentials and try again.', data: [] }, 400);
+		}
+		const token = generateToken();
+		await prisma.userForgot.create({
+			data: {
+				email,
+				token,
+				userId: user.id,
+			},
+		});
+
+		await fetch('https://api.postmarkapp.com/email/withTemplate', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'X-Postmark-Server-Token': '684fa8ef-2acb-452d-a2ca-6af9e071381e',
+			},
+			body: JSON.stringify({
+				From: 'pablo@typeauth.com',
+				To: email,
+				TemplateAlias: 'password-reset',
+				TemplateModel: {
+					email: email,
+					token: token,
+				},
+			}),
+		});
+
+		return c.json({ success: true, message: 'Password reset successfully', data: [] }, 200);
+	} catch (error) {
+		if (error instanceof SyntaxError) {
+			return c.json({ success: false, message: 'Invalid JSON syntax', data: [] }, 400);
+		} else if (error instanceof PrismaClientKnownRequestError) {
+			console.error('Prisma database error:', error);
+			return c.json({ success: false, message: 'Database error', data: [] }, 500);
+		} else {
+			console.error('Unexpected error:', error);
+			return c.json({ success: false, message: 'Internal server error', data: [] }, 500);
+		}
+	}
+});
+
+app.post('/reset-password', authMiddleware, async (c) => {
+	const prisma = createPrismaClient(c.env.DB);
+
+	try {
+		const { token, password } = await c.req.json();
+
+		if (!token || !password) {
+			return c.json({ success: true, message: 'Missing password or token', data: [] }, 400);
+		}
+
+		const userForgot = await prisma.userForgot.findUnique({
+			where: { token: token },
+			include: { user: true },
+		});
+		if (!userForgot) {
+			return c.json({ success: true, message: 'Invalid token. Please check your credentials and try again.', data: [] }, 400);
+		}
+		const hash = await hashPassword(password);
+		const userUpdate = await prisma.user.update({
+			where: { id: userForgot.userId },
+			data: { password: hash },
+		});
+
+		await fetch('https://api.postmarkapp.com/email/withTemplate', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'X-Postmark-Server-Token': '684fa8ef-2acb-452d-a2ca-6af9e071381e',
+			},
+			body: JSON.stringify({
+				From: 'pablo@typeauth.com',
+				To: userForgot.email,
+				TemplateAlias: 'reset-confirmation',
+				TemplateModel: {
+					email: userForgot.email,
+				},
+			}),
+		});
+
+		return c.json({ success: true, message: 'Password reset successfully', data: [] }, 200);
 	} catch (error) {
 		if (error instanceof SyntaxError) {
 			return c.json({ success: false, message: 'Invalid JSON syntax', data: [] }, 400);
@@ -189,214 +336,6 @@ app.post('/email/verify', authMiddleware, async (c) => {
 			console.error('Unexpected error:', error);
 			return c.json({ success: false, message: 'Internal server error' }, 500);
 		}
-	}
-});
-
-//? Retrieve a key by ID
-app.get('/:keyId', authMiddleware, async (c) => {
-	const prisma = createPrismaClient(c.env.DB);
-	try {
-		const appId = c.req.param('appId');
-		const keyId = c.req.param('keyId');
-		const accID = c.req.param('accId');
-		const key = await prisma.key.findUnique({
-			where: { id: keyId, accId: accID },
-			include: { Application: true },
-		});
-		if (!key || key.Application.id !== appId) {
-			return c.json({ success: true, message: 'Key not found,', data: [] }, 404);
-		}
-		const id = c.env.REMAINING.idFromName(key.value);
-		const bucket = c.env.REMAINING.get(id);
-		const response = await bucket.fetch(new Request(`https://remainer?key=${key.value}&get=true`));
-		const jsonResponse: { remaining: number } = await response.json();
-		key.remaining = jsonResponse.remaining;
-		key.refill = JSON.parse(key.refill!);
-		key.ratelimit = JSON.parse(key.ratelimit!);
-		key.Application.ratelimit = JSON.parse(key.Application.ratelimit!);
-		key.Application.refill = JSON.parse(key.Application.refill!);
-		return c.json({ success: true, message: '', data: [key] });
-	} catch (error) {
-		console.error('Unexpected error:', error);
-		return c.json({ success: false, message: 'Internal server error', data: [] }, 500);
-	}
-});
-
-//? Retrieve keys by appId
-app.get('', authMiddleware, async (c) => {
-	const prisma = createPrismaClient(c.env.DB);
-
-	try {
-		const appId = c.req.param('appId');
-		const accID = c.req.param('accId');
-		// Check if the appId exists
-		const application = await prisma.application.findUnique({
-			where: { id: appId },
-		});
-
-		if (!application) {
-			return c.json({ error: 'Application not found' }, 404);
-		}
-
-		// Retrieve all keys associated with the appId
-		const keys = await prisma.key.findMany({
-			where: { appId: appId, accId: accID },
-			select: {
-				id: true,
-				appId: true,
-				value: true,
-				enabled: true,
-			},
-		});
-
-		return c.json({ success: true, message: keys });
-	} catch (error) {
-		console.error('Error retrieving keys:', error);
-		return c.json({ success: false, message: 'Internal server error' }, 500);
-	}
-});
-
-//? Update a key
-app.post('/:keyId', authMiddleware, async (c) => {
-	const prisma = createPrismaClient(c.env.DB);
-
-	try {
-		const appId = c.req.param('appId');
-		const keyId = c.req.param('keyId');
-		const { value, environment, expires, metadata, ratelimit, refill } = await c.req.json();
-
-		const key = await prisma.key.findUnique({
-			where: { id: keyId },
-			include: { Application: true },
-		});
-
-		if (!key || key.Application.id !== appId) {
-			return c.json({ success: false, message: 'Key not found' }, 404);
-		}
-
-		const updatedKey = await prisma.key.update({
-			where: { id: keyId },
-			data: {
-				value,
-				environment,
-				expires,
-				metadata,
-				ratelimit,
-				refill,
-			},
-		});
-
-		return c.json({ success: true, message: updatedKey });
-	} catch (error) {
-		if (error instanceof SyntaxError) {
-			return c.json({ success: false, message: 'Invalid JSON syntax' }, 400);
-		} else if (error instanceof PrismaClientKnownRequestError) {
-			console.error('Prisma database error:', error);
-			return c.json({ success: false, message: 'Database error' }, 500);
-		} else {
-			console.error('Unexpected error:', error);
-			return c.json({ success: false, message: 'Internal server error' }, 500);
-		}
-	}
-});
-
-//? Update a key's remaining limit
-app.put('/:keyId/refill', authMiddleware, async (c) => {
-	const prisma = createPrismaClient(c.env.DB);
-
-	try {
-		const appId = c.req.param('appId');
-		const keyId = c.req.param('keyId');
-		const accID = c.req.param('accId');
-
-		const { refill } = await c.req.json();
-
-		const key = await prisma.key.findUnique({
-			where: { id: keyId, accId: accID, appId: appId },
-			include: { Application: true },
-		});
-
-		if (!key || key.Application.id !== appId) {
-			return c.json({ success: true, message: 'Key not found', data: [] }, 404);
-		}
-
-		const updatedKey = await prisma.key.update({
-			where: { id: keyId, accId: accID, appId: appId },
-			data: {
-				remaining: refill,
-			},
-		});
-
-		return c.json({ success: true, message: '', data: [updatedKey] });
-	} catch (error) {
-		if (error instanceof SyntaxError) {
-			return c.json({ success: false, message: 'Invalid JSON syntax' }, 400);
-		} else if (error instanceof PrismaClientKnownRequestError) {
-			console.error('Prisma database error:', error);
-			return c.json({ success: false, message: 'Database error' }, 500);
-		} else {
-			console.error('Unexpected error:', error);
-			return c.json({ success: false, message: 'Internal server error' }, 500);
-		}
-	}
-});
-
-//? Delete a key
-app.delete('/:keyId/delete', authMiddleware, async (c) => {
-	const prisma = createPrismaClient(c.env.DB);
-
-	try {
-		const appId = c.req.param('appId');
-		const keyId = c.req.param('keyId');
-		const accID = c.req.param('accId');
-
-		const key = await prisma.key.findUnique({
-			where: { id: keyId, accId: accID, appId: appId },
-			include: { Application: true },
-		});
-
-		if (!key || key.Application.id !== appId) {
-			return c.json({ success: true, message: 'Key not found', data: [] }, 404);
-		}
-
-		await prisma.key.delete({ where: { id: keyId } });
-
-		return c.json({ success: true, message: 'Key deleted successfully', data: [] });
-	} catch (error) {
-		console.error('Unexpected error:', error);
-		return c.json({ success: false, message: 'Internal server error', data: [] }, 500);
-	}
-});
-
-//* Retrieve usage numbers
-app.get('/:keyId/usage', authMiddleware, async (c) => {
-	const prisma = createPrismaClient(c.env.DB);
-
-	try {
-		const appId = c.req.param('appId');
-		const keyId = c.req.param('keyId');
-
-		const key = await prisma.key.findUnique({
-			where: { id: keyId },
-			include: { Application: true },
-		});
-
-		if (!key || key.Application.id !== appId) {
-			return c.json({ success: false, message: 'Key not found' }, 404);
-		}
-
-		// Retrieve usage numbers from the database or any other tracking mechanism
-		const usage = {
-			// Placeholder data, replace with actual usage numbers
-			total: 1000,
-			remaining: key.remaining,
-			consumed: 1000 - key.remaining!,
-		};
-
-		return c.json({ success: true, message: usage });
-	} catch (error) {
-		console.error('Unexpected error:', error);
-		return c.json({ success: false, message: 'Internal server error' }, 500);
 	}
 });
 
